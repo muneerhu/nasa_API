366af1a4b5a22db38403989fef2790cf041fde39
muneerhu <muneer.hu@gmail.com>
2020-12-07 11:58:14 -0500

    added test03

--- src/test/java/nasa_test/NASA_APOD_Test.java
+++ src/test/java/nasa_test/NASA_APOD_Test.java
@@ -3,13 +3,16 @@ package nasa_test;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class NASA_APOD_Test {
     String baseURI = ConfigReader.readProperty("baseURI", "src/test/resources/properties/env.properties");
     String API_KEY = ConfigReader.readProperty("api_key", "src/test/resources/properties/env.properties");
-
+    public static final String CURRENT_DATE = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     /**
      * Sending a get request without api key and because it is a required query param
      * request denied.
@@ -36,4 +39,23 @@ public class NASA_APOD_Test {
                 .log().body();
     }
 
+    /**
+     * for sending any request api key as query param is required but concept_tags, date, hd
+     * are optional
+     */
+
+    @Test(description = "Test with valid API_key and all other query params")
+    public void test03(){
+        given()
+                .when()
+                .queryParam("api_key", API_KEY)
+                .queryParam("date", CURRENT_DATE)
+                .queryParam("concept_tags", "true")
+                .queryParam("hd", "true")
+                .get(baseURI)
+                .then()
+                .statusCode(200)
+                .log().body();
+
+    }
 }
