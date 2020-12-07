76187998bde4bf1befa6b74c75781e02ec6b550f
muneerhu <muneer.hu@gmail.com>
2020-12-07 11:41:49 -0500

    added test01

--- src/test/java/nasa_test/NASA_APOD_Test.java
+++ src/test/java/nasa_test/NASA_APOD_Test.java
@@ -1,5 +1,27 @@
 package nasa_test;
 
-public class NASA_APOD_Test {
+import org.testng.annotations.Test;
+import utils.ConfigReader;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class NASA_APOD_Test {
+    String baseURI = ConfigReader.readProperty("baseURI", "src/test/resources/properties/env.properties");
+
+
+    /**
+     * Sending a get request without api key and because it is a required query param
+     * request denied.
+     */
+    @Test(description = "Test without API_key")
+    public void test01(){
+        given()
+                .when()
+                .get(baseURI)
+                .then()
+                .statusCode(403)
+                .body("error.code", equalTo("API_KEY_MISSING"))
+                .log().body();
+    }
 }
